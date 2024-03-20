aacfd229ed908eb22c1ffe063cc8fbc8785244d1
Arthur Sobreira <tutusobreira@gmail.com>
2024-03-20 13:20:01 -0300

    Update main.c

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,16 +6,18 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:27:39 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/03/19 15:52:04 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/03/20 12:04:16 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-	printf("Hello, philosophers!\n");
-	return (0);
+	if (validate_args(argc, argv))
+	{
+		// start_dinner();
+		return (EXIT_SUCCESS);
+	}
+	return (EXIT_FAILURE);
 }
