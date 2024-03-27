8854c6f4a3cc709a8b5df285be117fd28a1d50d4
Arthur Sobreira <tutusobreira@gmail.com>
2024-03-27 12:31:54 -0300

    Update defines header

--- philo/includes/defines.h
+++ philo/includes/defines.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 12:36:41 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/03/25 10:13:47 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/03/27 12:27:18 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,34 @@
 # define RESET		"\033[0m"
 
 /* Error Messages */
-# define ERROR_MSG 	RED_UNDER"Error:"RESET
-# define FEW_ARGS 	ERROR_MSG RED" Too few arguments :(\n"RESET
-# define MANY_ARGS	ERROR_MSG RED" Too many arguments :(\n"RESET
-# define NO_NUM		ERROR_MSG RED" Argument is not a number :(\n"RESET
-# define ZERO_NUM	ERROR_MSG RED" Argument is zero :(\n"RESET
-# define NEG_NUM 	ERROR_MSG RED" Argument is a negative number :(\n"RESET
-# define BIG_NUM 	ERROR_MSG RED" Argument is too big :(\n"RESET
-# define OPEN_SBRACKET	CYAN_BOLD"["RESET
-# define CLOSE_SBRACKET	CYAN_BOLD"]"RESET
+# define ERROR_MSG "\033[4;31mError:\033[0m "
+# define FEW_ARGS  "\033[0;31m Too few arguments :(\033[0m\n"
+# define MANY_ARGS "\033[0;31m Too many arguments :(\033[0m\n"
+# define NO_NUM    "\033[0;31m Argument is not a number :(\033[0m\n"
+# define ZERO_NUM  "\033[0;31m Argument is zero :(\033[0m\n"
+# define NEG_NUM   "\033[0;31m Argument is a negative number :(\033[0m\n"
+# define BIG_NUM   "\033[0;31m Argument is too big :(\033[0m\n"
+# define OPEN_SBRACKET "\033[1;36m[\033[0m"
+# define CLOSE_SBRACKET "\033[1;36m]\033[0m"
+
+/* Utils */
+# define VARS_COUNT	6
+
+typedef enum e_bool
+{
+	FALSE,
+	TRUE
+}				t_bool;
+
+enum e_vars
+{
+	M_PHILO_DEAD,
+	M_START_TIME,
+	M_PHILO_COUNT,
+	M_TIME_TO_DIE,
+	M_TIME_TO_EAT,
+	M_TIME_TO_SLEEP,
+	M_EAT_COUNT
+};
 
 #endif
