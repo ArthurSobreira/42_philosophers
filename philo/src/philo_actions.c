e7b45bdff3c46df1a02dcc19bfcac8c3d7c64d4b
Arthur <tutusobreira@gmail.com>
2024-03-30 17:28:24 -0300

    Update philo_actions.c

--- philo/src/philo_actions.c
+++ philo/src/philo_actions.c
@@ -22,9 +22,43 @@ void	philo_eat(t_philo *philo)
 	take_forks(philo);
 	print_status(philo, TAKE_FORK);
 	print_status(philo, TAKE_FORK);
+	pthread_mutex_lock(&data->m_vars[M_LAST_EAT]);
+	philo->last_eat = getter_current_time();
+	pthread_mutex_unlock(&data->m_vars[M_LAST_EAT]);
+	print_status(philo, EATING);
+	usleep(getter_time_to_eat() * 1000);
+	pthread_mutex_lock(&data->m_vars[M_EAT_COUNT]);
+	philo->eat_count++;
+	pthread_mutex_unlock(&data->m_vars[M_EAT_COUNT]);
+	drop_forks(philo);
 }
 
-// void	take_forks(t_philo *philo)
-// {
+void	take_forks(t_philo *philo)
+{
+	(void)philo;
+	if (getter_philo_death())
+		return ;
+}
 
-// }
+void	drop_forks(t_philo *philo)
+{
+	(void)philo;
+	if (getter_philo_death())
+		return ;
+}
+
+void	philo_sleep(t_philo *philo)
+{
+	if (getter_philo_death())
+		return ;
+	print_status(philo, SLEEPING);
+	usleep(getter_time_to_sleep() * 1000);
+}
+
+void	philo_think(t_philo *philo)
+{
+	if (getter_philo_death())
+		return ;
+	print_status(philo, THINKING);
+	usleep(1000);
+}
