9adab5f124be975e174f211f414612c470cee963
Arthur <tutusobreira@gmail.com>
2024-04-03 21:08:53 -0300

    Update bonus header

---
+++ philo_bonus/includes/philosophers_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers_bonus.h                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 21:00:39 by arsobrei          #+#    #+#             */
+/*   Updated: 2024/04/03 21:00:39 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_BONUS_H
+# define PHILOSOPHERS_BONUS_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <signal.h>
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <semaphore.h>
+# include "utils.h"
+
+#endif
