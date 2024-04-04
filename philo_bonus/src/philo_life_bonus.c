7a2e766441e8fb234979015a7a9bb09d1edcd470
Arthur Sobreira <tutusobreira@gmail.com>
2024-04-04 18:46:46 -0300

    Update philo_life_bonus.c

--- philo_bonus/src/philo_life_bonus.c
+++ philo_bonus/src/philo_life_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 14:44:06 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/04/04 15:46:37 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/04/04 18:42:22 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,38 @@ void	philo_life(t_philo *philo)
 	if (data->philo_count == 1)
 	{
 		memento_mori(philo);
-		exit(EXIT_SUCCESS);
+		exit(EXIT_FAILURE);
 	}
-	// while (TRUE)
-	// {
-	// 	philo_eat(philo);
-	// 	philo_sleep(philo);
-	// 	philo_think(philo);
-	// }
+	while (TRUE)
+	{
+		philo_eat(philo);
+		philo_sleep(philo);
+		philo_think(philo);
+	}
+}
+
+void	monitoring(t_philo *philo)
+{
+	t_data	*data;
+	t_bool	someone_died;
+	t_bool	eat_enough;
+	int		status;
+
+	data = get_data();
+	status = EXIT_SUCCESS;
+	someone_died = (get_current_time() - philo->last_eat \
+		> data->time_to_die);
+	eat_enough = (philo->eat_count >= data->total_eat_count);
+	if (someone_died || eat_enough)
+	{
+		if (someone_died)
+		{
+			print_status(philo, DIED);
+			status = EXIT_FAILURE;
+		}
+		close_semaphores();
+		exit(status);
+	}
 }
 
 void	end_philos(t_data *data)
@@ -36,7 +60,6 @@ void	end_philos(t_data *data)
 
 	index = 0;
 	free(data->philos_array);
-	sem_close(data->print);
-	sem_close(data->forks);
+	close_semaphores();
 	unlink_semaphores();
 }
